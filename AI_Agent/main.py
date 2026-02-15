7ca67fe9f25fd2923850c9b66a8e14a7c343f59f
Swetha <101114096+SwethaGot@users.noreply.github.com>
2026-02-15 23:15:45 +1100

    Added the agent API key

---
+++ AI_Agent/main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+# from langchain.agents import create_tool_calling_agent, AgentExecutor
+# from tools import search_tool, wiki_tool, save_tool
+
+load_dotenv()
+
+llm = ChatAnthropic(model="claude-3-5-sonnet-2024")
+response = llm.invoke("What is a TV?")
+print(response)      
